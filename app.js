1256ce1b3df6b18edf75ad8eed32cf4c95748e2a
Ross DiBartolo <dibartoloross@gmail.com>
2023-02-02 12:43:21 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ console.log(
 );
 
 // Exercise 1
+const firstName = `Ross`;
+let lastName = `Anthony`;
+var age = 33;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`)
 
 // Exercise 2
+lastName = `DiBartolo`;
+age = 34;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`)
 
 // Exercise 3
+let language = `JavaScript`;
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was started in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = `M`;
+
+console.log(`This shirt is size ${selectedSize} and is ${price}.`);
+
+let title = `Name of the Wind`;
+let author = `Patrick Rothfuss`;
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`"${title}" was written by ${author}. It is ${pageCount} pages long.`)
